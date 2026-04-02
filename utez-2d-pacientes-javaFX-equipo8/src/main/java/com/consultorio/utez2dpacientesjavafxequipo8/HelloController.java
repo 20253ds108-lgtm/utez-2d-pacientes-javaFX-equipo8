d15cb0651a7dbb23582a53217add5a87d5d49e62
unknown <20253ds108-lgtm>
2026-04-01 23:38:58 -0600

    creacion de la interfaz

--- utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/HelloController.java
+++ utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/HelloController.java
@@ -4,20 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class HelloController {
-    @FXML
-    private TextField txtNombre;
-    @FXML
-    private Label mensaje;
-
-    public void prueba(){ //pruebas inicialeS
-        String nombre=txtNombre.getText();
-        if (nombre.isEmpty()){
-            mensaje.setText("El nombre esta vacio");
-        }else {
-            mensaje.setText("el nombre tiene algo");
-        }
-
-    }
 
 }
---
+++ utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/service/personaService.java
@@ -0,0 +1,9 @@
+package com.consultorio.utez2dpacientesjavafxequipo8.service;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class personaService {
+
+}
