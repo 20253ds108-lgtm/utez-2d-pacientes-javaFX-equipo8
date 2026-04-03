66491eda21f3170301ca813d1cbb53a17a5c855f
unknown <20253ds108-lgtm>
2026-04-02 18:23:57 -0600

    creacion de personaservice, archivoRepositorio, paciente.csv, (aun falta el tableview)

--- utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/HelloController.java
+++ utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/HelloController.java
@@ -1,11 +1,83 @@
 package com.consultorio.utez2dpacientesjavafxequipo8;
 
+import com.consultorio.utez2dpacientesjavafxequipo8.service.personaService;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
+    @FXML
+    private TextField txtCurp;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtEdad;
+    @FXML
+    private TextField txtTel;
+    @FXML
+    private TextField txtAlergias;
+    @FXML
+    private TextField txtEstatus;
+    @FXML
+    private TableView<String> tableView;
+    @FXML
+    private Label lblAlertas;
+    @FXML
+    private TableColumn vistaCurp;
+    @FXML
+    private TableColumn vistaNombre;
+    @FXML
+    private TableColumn vistaEdad;
+    @FXML
+    private TableColumn vistaTel;
+    @FXML
+    private TableColumn vistaAlergias;
+    @FXML
+    private TableColumn vistaEstatus;
 
+    @FXML
+    public void initialize(){
+        tableView.getSelectionModel().selectedIndexProperty().addListener((observable,oldValue,newValue)->{
+            loadDataFromFile(newValue.toString());
+        });
+        tableView.setItems(data);
+        cargarArchivo();
+    }
+
+    @FXML
+    private personaService service = new personaService();
+    private final ObservableList<String> data = FXCollections.observableArrayList();
+
+    private void cargarArchivo(){//tratar de traer los archivos
+        try {
+            List<String> objeto = service.loadDataForList();
+            data.addAll(objeto);
+            lblAlertas.setText("se cargaron los datos correctamente");
+            lblAlertas.setStyle("-fx-text-fill: green");
+        }catch (IOException e){
+            lblAlertas.setText("error: no se cargaron los datos");
+            lblAlertas.setStyle("-fx-text-fill: red");
+
+        }
+    }
+
+    private void loadDataFromFile(String objeto){//cargar la data desde el archivo
+        if (objeto==null || objeto.isBlank()){
+            return;
+        }
+        String[] arreglo=objeto.split(",");
+        vistaCurp.setText(arreglo[0]);//arreglo para guardar todos los datos
+        vistaNombre.setText(arreglo[1]);
+        vistaEdad.setText(arreglo[2]);
+        vistaTel.setText(arreglo[3]);
+        vistaAlergias.setText(arreglo[4]);
+        vistaEstatus.setText(arreglo[5]);
+
+
+    }
 }
--- utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/service/personaService.java
+++ utez-2d-pacientes-javaFX-equipo8/src/main/java/com/consultorio/utez2dpacientesjavafxequipo8/service/personaService.java
@@ -1,9 +1,30 @@
 package com.consultorio.utez2dpacientesjavafxequipo8.service;
 
+import com.consultorio.utez2dpacientesjavafxequipo8.Repositorio.archivoRepositorio;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class personaService {
+    private final archivoRepositorio repos = new archivoRepositorio();
 
+    public List<String> loadDataForList() throws IOException {
+        List<String> lines = repos.readAllLines(); //lee todas las lineas que estan en pacientes.cvs
+        List<String> result = new ArrayList<>(); //en esta linea se las asigna
+        for (String line : lines){ //: recorre todas las lineas hasta que acaben
+            if (line==null || line.isBlank() ){//ignora las lineas que estan vacias
+            continue;
+            }
+            String[] arreglo = line.split(",",-1); //esta linea almacena los datos que si llrgan a tene algo (separados por una , )
+            String curp = arreglo[0].trim();//obtiene el curp
+            String nombre = arreglo[1].trim();//obtiene el nombre
+            String edad = arreglo[2].trim();//obtiene la edad
+            String tel = arreglo[3].trim();//obtiene el telefono
+            String alergias = arreglo[4].trim();//obtiene las alergias
+            String estatus = arreglo[5].trim();//obtiene el estatus
+            result.add(curp+" , "+nombre+" , "+edad+" , "+tel+" , "+alergias+" , "+estatus);//en este orden se mostraran los resultados en la tabla de pacientes
+        }
+        return  result;
+    }
 }
+
